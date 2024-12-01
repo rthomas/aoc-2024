c30589a50b29ff96cdaa933d2088d24a38a9bcc9
Ryan Thomas <ryan@ryant.org>
2024-12-02 08:38:50 +1100

    Puzzle 1 part 2

--- puzzle-1/src/main.rs
+++ puzzle-1/src/main.rs
@@ -1,7 +1,50 @@
-use std::{error::Error, fs::read_to_string};
+use std::{collections::HashMap, error::Error, fs::read_to_string};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = read_input("input")?;
+    let (left, right) = input()?;
+
+    part_1(left.clone(), right.clone())?;
+    part_2(left, right)?;
+
+    Ok(())
+}
+
+fn part_1(mut left: Vec<i32>, mut right: Vec<i32>) -> Result<(), Box<dyn Error>> {
+    left.sort();
+    right.sort();
+
+    let mut acc = 0;
+    for (i, j) in left.iter().zip(right.iter()) {
+        acc += i32::abs(i - j);
+    }
+
+    println!("Part 1: {acc}");
+
+    Ok(())
+}
+
+fn part_2(left: Vec<i32>, right: Vec<i32>) -> Result<(), Box<dyn Error>> {
+    let mut r_count = HashMap::new();
+
+    for j in right {
+        *r_count.entry(j).or_insert(0) += 1;
+    }
+
+    let mut acc = 0;
+    for i in left {
+        acc += i * r_count.get(&i).unwrap_or(&0);
+    }
+
+    println!("Part 2: {acc}");
+
+    Ok(())
+}
+
+fn input() -> Result<(Vec<i32>, Vec<i32>), Box<dyn Error>> {
+    let input = read_to_string("input")?
+        .lines()
+        .map(String::from)
+        .collect::<Vec<String>>();
 
     let mut left = Vec::with_capacity(input.len());
     let mut right = Vec::with_capacity(input.len());
@@ -16,22 +59,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         right.push(lr[1]);
     }
 
-    left.sort();
-    right.sort();
-
-    let mut acc = 0;
-    for (i, j) in left.iter().zip(right.iter()) {
-        acc += i32::abs(i - j);
-    }
-
-    println!("{acc}");
-
-    Ok(())
-}
-
-fn read_input(filename: &str) -> Result<Vec<String>, std::io::Error> {
-    Ok(read_to_string(filename)?
-        .lines()
-        .map(String::from)
-        .collect())
+    Ok((left, right))
 }
